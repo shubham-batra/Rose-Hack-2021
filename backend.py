7d3cfd55acddc0eec06639969913d7caebd56fea
shubham-batra <shubham.batra@outlook.com>
2021-01-16 21:05:10 -0800

    added python file

---
+++ backend.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host= capable-conduit-302002:us-central1:music-database
+  user="yourusername",
+  password="yourpassword"
+)
+
+print(mydb)
