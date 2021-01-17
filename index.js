7a9993c27c0c745f789790c1b72f3187a9e54d0e
shubham-batra <shubham.batra@outlook.com>
2021-01-16 23:16:00 -0800

    Able to remove albums now

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 myArray = new Array();
 var numberList = 0; // Album Increment
 
-    function showMessage(){ 
+    function showMessage(){ // Add Albums 
         document.getElementById("display_message").innerHTML = "";
         numberList = numberList+1;
         var artistField = document.getElementById("artistfield").value;
@@ -15,7 +15,7 @@ var numberList = 0; // Album Increment
         printArray();
     }
 
-    function deleteAlbum() {
+    function deleteAlbum() { // Delete Albums from Array
         var numberField = document.getElementById("numberfield").value;
         myArray.splice(numberField-1, 1); // Delete element here
         document.getElementById("display_message").innerHTML = "";
@@ -23,7 +23,7 @@ var numberList = 0; // Album Increment
 
     }
 
-    function printArray() {
+    function printArray() { // Generic function to print Array
         document.getElementById("display_message").innerHTML = "";
         for (i = 0; i < myArray.length; i++)
             document.getElementById("display_message").innerHTML += myArray[i];
