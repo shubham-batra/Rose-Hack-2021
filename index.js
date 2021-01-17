5421056aeff9ac41bf12f5a5655877977c119e94
shubham-batra <shubham.batra@outlook.com>
2021-01-16 18:20:05 -0800

    Fixed bug with entry

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+const myArray = new Array();
+
     function showMessage(){ 
+        document.getElementById("display_message").innerHTML = "";
         var artistField = document.getElementById("artistfield").value;
         var albumField = document.getElementById("albumfield").value;
         var ratingField = document.getElementById("ratingfield").value;
         console.log(artistField);
         console.log(albumField);
         console.log(ratingField);
-        var returnString = "Artist: " +  artistField + "   " + " Album: " + albumField + "   " + " Rating: " +ratingField;
-        return document.getElementById("display_message").innerHTML = returnString;
+        var returnString = "Artist: " +  artistField + "   " + " Album: " + albumField + "   " + " Rating: " +ratingField +"<br />";
+        myArray.push(returnString);
+        for (i = 0; i < myArray.length; i++)
+            document.getElementById("display_message").innerHTML += myArray[i];
     }
